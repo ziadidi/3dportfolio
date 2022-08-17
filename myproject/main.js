2886f4f2629229b7561e5948eb8747b3271bddb5
ziadidi <dianepucci19@gmail.com>
2022-08-17 19:10:29 +0100

    end

--- myproject/main.js
+++ myproject/main.js
@@ -36,6 +36,60 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star)
+}
+
+Array(200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+const dianciTexture = new THREE.TextureLoader().load('dianci.png');
+const dianci = new THREE.Mesh(
+new THREE.BoxGeometry(4,4,4),
+new THREE.MeshBasicMaterial({map: dianciTexture})
+);
+
+scene.add(dianci);
+
+const moonTexture = new THREE.TextureLoader().load('moon.png');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+new THREE.SphereGeometry(3,32,32),
+new THREE.MeshStandardMaterial({
+  map: moonTexture,
+  normalMap: normalTexture,
+})
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+  
+  dianci.rotation.y += 0.01;
+  dianci.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+
 function animate() {
   requestAnimationFrame( animate );
  torusKnot.rotation.x += 0.01;
